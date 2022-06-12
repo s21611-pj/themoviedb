b7dd8da6887fc34a87a35b2c7e021959b6e27d88
Wojciech Turek <s21611@pjwstk.edu.pl>
2022-06-13 00:04:27 +0200

    Popular Movies with Posters rendered in react-bootstrap grid

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,9 @@
-import { Card, Button } from 'react-bootstrap';
+import { Card, Container, Col, Row } from 'react-bootstrap';
 import React, { useEffect, useState } from "react"
 import * as Constants from '../constants/constants.js'
 
 export const MovieCard = () => {
-    
+
     const [movies, setMovies] = useState([])
 
     const fetchData = () => {
@@ -26,17 +26,18 @@ export const MovieCard = () => {
 
     return (
         <div>
-            {movies.map(movie =>
-                <Card style={{ width: '15rem' }}>
-                    <Card.Img variant="top" src="../logo192.png" width="100" height="200" />
-                    <Card.Body>
-                        <Card.Title>{movie.original_title}</Card.Title>
-                        <Card.Text>
-                            {movie.overview}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            )}
-        </div>
+            <Container className='p-4'>
+                    <Row>
+                        {movies.map(movie =>
+                            <Card style={{ width: '15rem' }} className="m-2">
+                                <Card.Img variant="top" src={ `${Constants.POSTER_URL}${movie.poster_path}` } width="100" height="200" />
+                                <Card.Body>
+                                    <Card.Title>{movie.original_title}</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        )}
+                    </Row>
+            </Container>
+        </div >
     )
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
-export const API_URL = "https://api.themoviedb.org/3/"
-export const POPULAR_MOVIES_ENDPOINT = "movie/popular/"
-export const API_KEY = "?api_key=75380d8826134e9a6c18ac863c44512f"
+export const API_URL = "https://api.themoviedb.org/3/";
+export const POPULAR_MOVIES_ENDPOINT = "movie/popular/";
+export const API_KEY = "?api_key=75380d8826134e9a6c18ac863c44512f";
+export const POSTER_URL = "https://image.tmdb.org/t/p/original";
